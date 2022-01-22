042039740a12b19b8cdb2e4c303aa4b579e54961
cahya <cahya@skyshi.com>
2022-01-22 10:37:57 +0700

    dlete console.log

--- src/task-3.js
+++ src/task-3.js
@@ -14,10 +14,5 @@ function countBox(...number) {
     return Math.max(...hcfEach);
 }
 
-const kue = 20;
-const apple = 25;
-
-const box = countBox(apple,kue);
-console.log(`Banyaknya kotak yang dapat dibuat oleh Ainun dengan membagi rata masing masing item per kotak adalah ${box} kotak`)
 
 module.exports= countBox;
